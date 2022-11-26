5193efd5b7c98f7bbd706dbe1ba1342e00451584
gisella de los rios <gis1097@hotmail.com>
2022-11-26 11:39:52 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const userRoutes = require('./routes/users.routes');
+const categoriesRoutes = require('./routes/categories.routes');
+const coursesRoutes = require('./routes/courses.routes');
+const videosRoutes = require('./routes/videos.routes');
+const usersCoursesRoute = require('./routes/usersCourses.routes')
+const morgan = require ('morgan');
+const db = require('./utils/database');
+const initModels = require('./models/initModels');
+const app = express();
+
+initModels()
+
+app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+const PORT = 8000;
+
+db.authenticate()
+  .then(() => console.log('autenticacion exitosa') )
+  .catch((error) => console.log(error));
+
+  db.sync({force : false})
+  .then(()=>{console.log('DB sincronizada')})
+  .catch((error)=> {console.log(error)});
+
+
+app.use('/api/v1', usersCoursesRoute);
+app.use('/api/v1', userRoutes);
+app.use('/api/v1', categoriesRoutes);
+app.use('/api/v1', coursesRoutes);
+app.use('/api/v1', videosRoutes);
+
+console.log("MIPUERTO ES  " + process.env.PORT);
+
+app.listen(PORT)
+
+module.exports = app;
+
+
